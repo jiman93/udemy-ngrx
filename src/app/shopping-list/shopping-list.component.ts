9cfef482efcfaeb455b3d4ba462d8e2c9a52b5d4
unknown <zulhafiz06@gmail.com>
2018-11-12 00:57:20 +0800

    first commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,42 @@
+import { Ingredient } from './../shared/ingredient.model';
+import { Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { Subscription } from 'rxjs/Subscription';
+
+import { ShoppingListService } from './shopping-list.service';
+import { Observable } from 'rxjs';
+import * as fromShoppingList from './store/shopping-list.reducers';
+import * as ShoppingListActions from './store/shopping-list.actions';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  shoppingListState: Observable<{ingredients: Ingredient[]}>;
+  // private subscription: Subscription;
+
+  // constructor(private slService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
+  constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
+
+  ngOnInit() {
+    this.shoppingListState = this.store.select('shoppingList');
+    console.log(this.store);
+    // this.subscription = this.slService.ingredientsChanged
+    //   .subscribe(
+    //     (ingredients: Ingredient[]) => {
+    //       this.ingredients = ingredients;
+    //     }
+    //   );
+  }
+
+  onEditItem(index: number) {
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
+  }
+
+  // ngOnDestroy() {
+  //   this.subscription.unsubscribe();
+  // }
+}
